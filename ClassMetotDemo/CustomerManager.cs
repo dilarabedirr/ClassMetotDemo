177b2d7e088f1cd08a20a0d5e9bf789cf0fcba7d
Dilara Bedir <dilara_bedir@hotmail.com>
2021-02-13 14:44:05 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("==>Müşteri Bilgileri Eklendi.");
+        }
+        public void Update()
+        {
+            Console.WriteLine("==>Müşteri Bilgileri Güncellendi.\n");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("==>Müşteri Bilgileri Silindi.");
+        }
+        public void List()
+        {
+            Console.WriteLine("==>Müşteri Bilgileri Listelendi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Firstname = "Ali";
+            customer1.Lastname = "Yıldırım";
+            customer1.City = "İstanbul";
+
+            Customer customer2 = new Customer();
+            customer2.Firstname = "Mislina";
+            customer2.Lastname = "Karadeniz";
+            customer2.City = "Ankara";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            foreach (Customer customer in customers)
+            {
+                customerManager.Add();
+                customerManager.List();
+                Console.WriteLine("\nMüşteri Adı: " + customer.Firstname + "\nMüşteri Soyadı: " + customer.Lastname + "\nŞehir: " + customer.City + "\n--------------------------------");
+                customerManager.Update();
+            }
+            customerManager.Delete();
+        }
+    }
+}
